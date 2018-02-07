7f3a4c9e4c095695abd6c66c4362b220d6b82ec6
linianjun <lljit@qq.com>
2018-02-07 17:59:08 +0800

    add security

---
+++ src/main/java/info/linianjun/security/controller/HelloController.java
@@ -0,0 +1,26 @@
+package info.linianjun.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping("/he")
+public class HelloController {
+	
+	@RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+}
